9fa685839b03bd5235aa6b5b2c354e76872a80c2
yale8848 <royal8848@163.com>
2017-11-16 17:08:50 +0800

    add some test url

---
+++ app/src/main/java/ren/yale/android/cachewebview/Main2Activity.java
@@ -0,0 +1,146 @@
+package ren.yale.android.cachewebview;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.File;
+
+import ren.yale.android.cachewebviewlib.utils.NetworkUtils;
+
+public class Main2Activity extends Activity {
+
+    private WebView mWebView;
+    public static final String KEY_URL="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        mWebView = findViewById(R.id.webview);
+
+        String url = getIntent().getStringExtra(KEY_URL);
+
+
+        mWebView.loadUrl(url);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+
+                Log.d("CacheWebView","shouldInterceptRequest: "+request.getUrl().toString());
+                return null;
+            }
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                Log.d("CacheWebView","shouldInterceptRequest: "+url);
+                return null;
+            }
+        });
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("CacheWebView",consoleMessage.message());
+                return super.onConsoleMessage(consoleMessage);
+            }
+        });
+        initSettings();
+
+    }
+
+
+    private void initSettings(){
+        WebSettings webSettings = mWebView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setUseWideViewPort(true);
+
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        webSettings.setSupportZoom(false);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setDisplayZoomControls(false);
+
+        webSettings.setDefaultTextEncodingName("UTF-8");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            webSettings.setAllowFileAccessFromFileURLs(true);
+            webSettings.setAllowUniversalAccessFromFileURLs(true);
+        }
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptThirdPartyCookies(mWebView,true);
+        }
+       if (NetworkUtils.isConnected(mWebView.getContext()) ){
+            webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        } else {
+            webSettings.setCacheMode(
+                    WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        }
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(
+                    WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        }
+        setCachePath();
+
+    }
+
+    private void setCachePath(){
+
+        File cacheFile = new File(mWebView.getContext().getCacheDir(),"appcache_name");
+        String path = cacheFile.getAbsolutePath();
+
+        File file = new File(path);
+        if (!file.exists()){
+            file.mkdirs();
+        }
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDatabasePath(path);
+    }
+}
